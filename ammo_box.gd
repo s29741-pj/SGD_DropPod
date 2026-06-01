55a4a2e4a93eb328c9d9de65465b6ed7fb94e0e1
s29741-pj <s29741@pjwstk.edu.pl>
2026-06-01 17:24:28 +0200

    Added ammobox and healthpack animation.

--- ammo_box.gd
+++ ammo_box.gd
@@ -1,14 +1,24 @@
 extends Area2D
 
+@onready var sfx_player = $SFXPlayer
+@export var sfx_pickup: AudioStream
 var ammo_amount = 10
 
 func _ready():
 	body_entered.connect(_on_body_entered)
+	$Sprite.play("idle")
 
 func _on_body_entered(body):
-	if body.has_method("pickup_ammo"):
+	if body.is_in_group("player"):
 		var current = body.weapons[body.current_weapon]
 		if current in body.ammo:
 			if body.ammo[current] < body.max_ammo[current]:
+				if sfx_pickup:
+					sfx_player.stream = sfx_pickup
+					sfx_player.play()
 				body.pickup_ammo(ammo_amount)
+				$CollisionShape2D.disabled = true
+				$Sprite.play("pickup")
+				await $Sprite.animation_finished
+				await get_tree().create_timer(0.5).timeout
 				queue_free()
--- healthpack.gd
+++ healthpack.gd
@@ -1,12 +1,21 @@
 extends Area2D
 
+@onready var sfx_player = $SFXPlayer
+@export var sfx_pickup: AudioStream
+
 var heal_amount = 2
 
 func _ready():
 	body_entered.connect(_on_body_entered)
+	$Sprite.play("idle")
 
 func _on_body_entered(body):
-	if body.has_method("heal"):
-		if body.hp < body.max_hp:
-			body.heal(heal_amount)
-			queue_free()
+	if body.is_in_group("player") and body.hp < body.max_hp:
+		if sfx_pickup:
+			sfx_player.stream = sfx_pickup
+			sfx_player.play()
+		body.heal(heal_amount)
+		$CollisionShape2D.disabled = true
+		$Sprite.play("pickup")
+		await $Sprite.animation_finished
+		queue_free()
